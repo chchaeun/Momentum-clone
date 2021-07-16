f31d0e91d51234628071171dbf6a6f847cf69276
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2021-07-16 17:44:29 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -7,6 +7,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 bgImage.classList.add("background");
 
-bgImage.src = `/img/${chosenImage}`;
+bgImage.src = `./img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
